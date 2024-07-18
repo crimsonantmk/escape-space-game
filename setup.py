bbc9b439bd32882c8c02a9dc914b3f5d5cd0118b
Antony Munyiri <144614414+crimsonantmk@users.noreply.github.com>
2024-07-18 20:49:10 +0300

    Update setup.py
    ne

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 import os
 
-# Dependencies are automatically detected, but it might need fine tuning.
+
 build_exe_options = {"packages": ["os", "pygame"]}
 
 # GUI applications require a different base on Windows (the default is for a
@@ -14,7 +14,7 @@ if sys.platform == "win32":
 pygame_py_file = os.path.join('spaceshooter', 'spaceShooter.py')
 
 ## The image and sound files are added manually into the zip file
-## A fix for this would be released
+
 
 setup(  name = "Space Shooter",
         version = "0.0.2",
